5ab1dc14437ecb85ef19a2168181298da85fa3dc
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2023-07-13 12:43:39 +0100

    Seventeeth commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,76 +2,79 @@
 
 import Cover from '@/components/Cover';
 import Universal from '@/components/Universal';
-import {useForm} from 'react-hook-form'
-import {DevTool} from '@hookform/devtools'
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 
 
 const Contact = () => {
 
-  const  form = useForm()
-  const {register,formState,control,handleSubmit} = form
-  const {errors} = formState
+  const form = useForm();
+  const { register, formState, control, handleSubmit } = form;
+  const { errors } = formState;
 
-  
-  
-  const onSubmit = (data: any)=>{
-console.log(data)
-  }
-
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
 
   return (
     <Universal>
       <Cover name="Contact Us" />
 
-      <section>
+      <div className="contact">
+
+        <section>
         <div className="container">
-          <h1>Our Contact Details</h1>
-          <p>
-            Locate us at our office today or use any of the contact details
-            below to get in touch with us today and we will get back to you
-            within 24 hours.
-          </p>
-        </div>
-      </section>
-      <section>
-        <div className="container">
-          <div>
-            <h2>We Love To Hear From You</h2>
+            <h1>Our Contact Details</h1>
+            <div className="short-line"></div>
             <p>
-              Please call or email contact form and we will be happy to assist you.
+              Locate us at our office today or use any of the contact details
+              below to get in touch with us today and we will get back to you
+              within 24 hours.
             </p>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            </div>
+        </section>
 
-              <div className="form-control">
-                <input type="text" id="fullname" {...register('fullname')} />
-                <label htmlFor="fullname">Your name</label>
-              </div>
-              
 
-              <div className="form-control">
-                <input type="text" id="email" {...register('email')} />
-                <label htmlFor="email">Your email</label>
-              </div>
+        <section>      
+          <div className="container">
 
-              <div className="form-control">
-                <input type="text" id="subject" {...register('subject')} />
-                <label htmlFor="">Subject</label>
-              </div>
+            <div>
+              <h2>We Love To Hear From You</h2>
+              <div className="short-line"></div>
+              <p>
+                Please call or email contact form and we will be happy to assist
+                you.
+              </p>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control">
+                  <input type="text" id="fullname" {...register('fullname')} />
+                  <label htmlFor="fullname">Your name</label>
+                </div>
 
-              <div className="form-control">
-                <input type="text" id="fullname" />
-                <label htmlFor=""></label>
-              </div>
+                <div className="form-control">
+                  <input type="text" id="email" {...register('email')} />
+                  <label htmlFor="email">Your email</label>
+                </div>
 
-              <button type="submit">Submit</button>
+                <div className="form-control">
+                  <input type="text" id="subject" {...register('subject')} />
+                  <label htmlFor="">Subject</label>
+                </div>
 
-            </form>
-            <DevTool control={control} />
-          </div>
-          <div></div>
-        </div>
-      </section>
+                <div className="form-control">
+                  <input type="text" id="fullname" />
+                  <label htmlFor=""></label>
+                </div>
+
+                <button type="submit">Submit</button>
+              </form>
+              <DevTool control={control} />
+            </div>
+            <div></div>
+            </div>
+        </section>
+      </div>
     </Universal>
   );
 };
