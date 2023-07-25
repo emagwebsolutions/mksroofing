7723db909a21adbc521c6ff315a8197a97371c88
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2023-07-25 14:35:47 +0100

    Twenty two commit

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -44,7 +44,7 @@ const Slider = () => {
           <div className="each-fade" key={index}>
             <Image
               priority
-              width="7000"
+              width="1300"
               height={400}
               alt=""
               src={fadeImage.url}
